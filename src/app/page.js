1564b0d757c530e887533f04223c87c8a412801e
wyn-code <brunoo6.massocco@gmail.com>
2025-06-28 17:14:10 -0300

    Initial proyect

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center items-center h-100">
+      <h1>
+        Hola dsajkdhas
+      </h1>
+    </div>
+    </>
+  );
+}
